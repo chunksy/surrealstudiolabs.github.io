21bf1cd2419d4bfb03783de67741fb46e0e17f16
chunksy <45887084+chunksy@users.noreply.github.com>
2025-11-10 20:24:33 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,17 @@
+function openTab(event, tabName) {
+            // Hide all tab content
+            var tabContent = document.getElementsByClassName("tab-content");
+            for (var i = 0; i < tabContent.length; i++) {
+                tabContent[i].classList.remove("active");
+            }
+
+            // Remove active class from all buttons
+            var tabButtons = document.getElementsByClassName("tab-button");
+            for (var i = 0; i < tabButtons.length; i++) {
+                tabButtons[i].classList.remove("active");
+            }
+
+            // Show the selected tab and mark button as active
+            document.getElementById(tabName).classList.add("active");
+            event.currentTarget.classList.add("active");
+        }
